924b3e4a60ee832a0b24927b04444ed77b7a37e1
Mircea Petre <wonka_s@yahoo.com>
2021-04-10 00:01:29 +0300

    test database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-solid-76998',
       user : 'postgres',
       password : 'papirus',
       database : 'faceapp'
